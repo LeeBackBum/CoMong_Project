16382e72785c337b24c5ee1818cf4690671211f6
Bum <fmsma1@naver.com>
2024-11-19 10:26:25 +0900

    1118

--- user/src/main/java/edu/sm/controller/UserController.java
+++ user/src/main/java/edu/sm/controller/UserController.java
@@ -2,26 +2,29 @@ package edu.sm.controller;
 
 import edu.sm.app.dto.UserDto;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import edu.sm.app.service.UserService;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
 
+    String dir = "user/";
+    final UserService userService;
+
     @RequestMapping("mypage")
-    public String myPage(Model model, HttpSession session) {
-        UserDto userDto = (UserDto) session.getAttribute("loginid"); // 세션에서 로그인된 사용자 정보 가져오기
-        if (userDto != null) {
-            model.addAttribute("user", userDto);
-        } else {
-            // 로그인하지 않은 경우, 로그인 페이지로 리다이렉트 또는 예외 처리
-            return "redirect:/login";
-        }
+    public String mypage(Model model, @RequestParam("id") String id) throws Exception {
+        UserDto userDto = null;
+        userDto = userService.get(id);
+        model.addAttribute("user", userDto);
+        model.addAttribute("center", dir + "mypage");
         return "index";
     }
 }
