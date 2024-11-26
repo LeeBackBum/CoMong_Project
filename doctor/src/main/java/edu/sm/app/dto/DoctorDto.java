3255e37460eafe2c6f31d7b007020e41d7ef2888
seunghyun <tmdgus8467@naver.com>
2024-11-26 15:55:33 +0900

    .

--- doctor/src/main/java/edu/sm/app/dto/DoctorDto.java
+++ doctor/src/main/java/edu/sm/app/dto/DoctorDto.java
@@ -1,10 +1,14 @@
 package edu.sm.app.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DoctorDto {
         private String doctorId;
         private String doctorPwd;
@@ -12,9 +16,10 @@ public class DoctorDto {
         private String doctorName;
         private String doctorAge;
         private String doctorEmail;
-        private String doctorImg;
+        private String doctorImg; // 업로드된 파일
         private String career;         // 경력
         private String ancestry;       // 학력
         private String society;        // 학회
         private String subjectName;    // 진료과목 이름
 }
+
