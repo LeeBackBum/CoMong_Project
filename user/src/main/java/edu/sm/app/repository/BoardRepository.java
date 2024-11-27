5ab7f992810c5153b07015ad2e9fad4ee259d9b3
seunghyun <tmdgus8467@naver.com>
2024-11-27 14:14:22 +0900

    주석처리

--- user/src/main/java/edu/sm/app/repository/BoardRepository.java
+++ user/src/main/java/edu/sm/app/repository/BoardRepository.java
@@ -10,14 +10,14 @@ import java.util.List;
 @Mapper
 public interface BoardRepository extends SMRepository<Integer, BoardDto> {
 
-    // 공지사항만 가져오기
-    List<BoardDto> selectNotices();
-
-    // 일반 게시글만 가져오기
-    List<BoardDto> selectGeneralBoards();
-
-    // 공지사항 포함 모든 게시글 가져오기 (공지사항 상단 정렬)
-    List<BoardDto> selectAllBoards();
+//    // 공지사항만 가져오기
+//    List<BoardDto> selectNotices();
+//
+//    // 일반 게시글만 가져오기
+//    List<BoardDto> selectGeneralBoards();
+//
+//    // 공지사항 포함 모든 게시글 가져오기 (공지사항 상단 정렬)
+//    List<BoardDto> selectAllBoards();
 
     // 조회수 증가
     void increaseHits(@Param("boardId") Integer boardId);
